2d38ce64bd406e15b2e204a6516b60879af1b25f
bosak <bosakmaw@gmail.com>
2017-10-21 21:24:35 +0300

    minimal working prototype

--- chatbot.py
+++ chatbot.py
@@ -7,10 +7,19 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 '''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
-import sys
-import irc.bot
+from threading import Thread
+from collections import deque
+
 import requests
+import irc.bot
+import numpy
+
+from pysc2.agents import base_agent
+from pysc2.lib import actions
 
 class TwitchBot(irc.bot.SingleServerIRCBot):
     prefix = '!'
@@ -19,6 +28,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         self.client_id = client_id
         self.token = token
         self.channel = '#' + channel
+        self.commands = deque()
 
         # Get the channel id, we will need this for v5 API calls
         url = 'https://api.twitch.tv/kraken/users?login=' + channel
@@ -31,43 +41,45 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         port = 6667
         print('Connecting to ' + server + ' on port ' + str(port) + '...')
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:'+token)], username, username)
-        
 
     def on_welcome(self, c, e):
-        print('Joining ' + self.channel)
-
-        # You must request specific capabilities before you can use them
         c.cap('REQ', ':twitch.tv/membership')
         c.cap('REQ', ':twitch.tv/tags')
         c.cap('REQ', ':twitch.tv/commands')
         c.join(self.channel)
+        print('Joined ' + self.channel)
 
     def on_pubmsg(self, c, e):
-
-        # If a chat message starts with an exclamation point, try to run it as a command
         if e.arguments[0][:1] == TwitchBot.prefix:
-            cmd = e.arguments[0].split(' ')[0][1:]
-            print('Received command: ' + cmd)
-            self.do_command(e, cmd)
+            cmd = e.arguments[0][1:]
+            print('> ' + cmd)
+            self.commands.append(cmd)
         return
 
-    def do_command(self, e, cmd):
-        c = self.connection
+    def has_commands(self):
+        return bool(self.commands)
 
-        # Poll the API to get current game.
-        if cmd == "game":
-            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
-            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
-            r = requests.get(url, headers=headers).json()
-            c.privmsg(self.channel, r['display_name'] + ' is currently playing ' + r['game'])
+    def next_command(self):
+        return self.commands.popleft()
 
-def main():
-    if len(sys.argv) != 5:
-        print("Usage: twitchbot <username> <client id> <token> <channel>")
-        sys.exit(1)
+class TwitchAgent(base_agent.BaseAgent):
+    def __init__(self):
+        super(TwitchAgent, self).__init__()
 
-    bot = TwitchBot(*sys.argv[1:5])
-    bot.start()
+        with open('.secrets', 'r') as secrets:
+            args = secrets.readline().split()
+            self.bot = TwitchBot(*args)
 
-if __name__ == "__main__":
-    main()
+        thread = Thread(target=self.bot.start)
+        thread.daemon = True
+        thread.start()
+
+    def step(self, obs):
+        super(TwitchAgent, self).step(obs)
+        if self.bot.has_commands():
+            line = self.bot.next_command().split(' ')
+            name = line[0]
+            function = actions.FUNCTIONS[name]
+            args = [[int(a) for a in arg.split(',')] for arg in line[1:]]
+            return actions.FunctionCall(function.id, args)
+        return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
